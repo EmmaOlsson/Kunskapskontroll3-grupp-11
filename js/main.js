587a9fbd3a1cfb8c52451fc90ca69d5de720de15
Emma Olsson <emma.olsson22@learnet.se>
2020-11-30 16:41:22 +0100

    kattbilder

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
 
-fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ede2a0d6e77e60346537e570cfab9800&gallery_id=50619632627&format=json&nojsoncallback=1?secret=1d5f8c611d8e8096')
+fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ede2a0d6e77e60346537e570cfab9800&gallery_id=72157717105466456&format=json&nojsoncallback=1?secret=1d5f8c611d8e8096?')
       .then(function(response){
   
         if(response.status >= 200 && response.status <300){
@@ -12,11 +12,23 @@ fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&a
       .then(
     
           function(data){
+            console.log(data)
+
+            let id = data.photos.photo[0].id;
+            let serverId = data.photos.photo[0].server;
+            let secret = data.photos.photo[0].secret;
+            let sizeSuffix = 'm';
+
+            let imageUrl = `url(https://live.staticflickr.com/${serverId}/${id}_${secret}_${sizeSuffix}.jpg)`;
+
+            console.log(imageUrl);
 
             let cit =document.querySelector('aside');
-            cit.style.backgroundImage=`url('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ede2a0d6e77e60346537e570cfab9800&gallery_id=50619632627&format=json&nojsoncallback=1?secret=1d5f8c611d8e8096')`;
+            cit.style.backgroundImage = imageUrl;
+            
+
+
 
           }
       )
     
-        
