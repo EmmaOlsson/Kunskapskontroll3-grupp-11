71dbda93900dce0fbcc63cbde63b2b722f1600b8
Emma Olsson <emma.olsson22@learnet.se>
2020-11-30 16:49:22 +0100

    Updated js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-const url = 'https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ede2a0d6e77e60346537e570cfab9800&gallery_id=72157716940730018&format=json&nojsoncallback=1'
 
-fetch(url).then(
-    response => {return response.json();}
-).then(
+
+
+fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ede2a0d6e77e60346537e570cfab9800&gallery_id=72157717105466456&format=json&nojsoncallback=1?secret=1d5f8c611d8e8096?')
+.then(function(response){
+
+  if(response.status >= 200 && response.status <300){
+      return response.json();
+  }
+
+})
+.then(
+
     function(data){
-        const img = document.querySelector()
-        console.log(data)
+      console.log(data)
+
+      let id = data.photos.photo[0].id;
+      let serverId = data.photos.photo[0].server;
+      let secret = data.photos.photo[0].secret;
+      let sizeSuffix = 'm';
+
+      let imageUrl = `url(https://live.staticflickr.com/${serverId}/${id}_${secret}_${sizeSuffix}.jpg)`;
+
+      console.log(imageUrl);
+
+      let cit =document.querySelector('aside');
+      cit.style.backgroundImage = imageUrl;
+      
+
+
 
     }
 )
