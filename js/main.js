6fe51e57cbdf91fafa3e11f5ae1a8e33f52aa41f
mamounalchalabi <70330996+mamounalchalabi@users.noreply.github.com>
2020-12-01 14:58:06 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,74 @@
-fetch('https://www.flickr.com/services/rest/?api_key=ede2a0d6e77e60346537e570cfab9800&method=flickr.photos.search&text=soda&format=json&nojsoncallback=1')
-.then(function(response){
 
-  if(response.status >= 200 && response.status <300){
+// Setting the api-key to a variable
+const keyAPI = 'ede2a0d6e77e60346537e570cfab9800';
+
+//Setting thhe secret-key to a variable
+const keySecret = '1d5f8c611d8e8096?';
+
+// Creating variables for different gallery-id:s
+const catGallery = '72157717105466456';
+const dogGallery = '72157717117782237';
+const horseGallery = '72157717118951277';
+
+// Selects and sets a variable for the gallery-buttons from HTML
+let catButton = document.querySelector('#cat-button')
+let dogButton = document.querySelector('#dog-button')
+let horseButton = document.querySelector('#horse-button')
+
+// Set this to a 
+function chooseGallery() {
+  return catGallery;
+}
+
+catButton.addEventListener('click', chooseGallery);
+
+
+
+// If the user clicks on cat, the urlGallery = catGallery
+let urlGallery = chooseGallery();
+
+let url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${keyAPI}&gallery_id=${urlGallery}&format=json&nojsoncallback=1?secret=${keySecret}`;
+
+
+fetch(url)
+  .then(function (response) {
+
+    if (response.status >= 200 && response.status < 300) {
       return response.json();
-  }
+    }
 
-})
-.then(
+  })
+  .then(
 
-    
-    function(data){
+    function (data) {
       console.log(data)
 
-      let id = data.photos.photo[0].id;
-      let serverId = data.photos.photo[0].server;
-      let secret = data.photos.photo[0].secret;
-      let sizeSuffix = 'm';
 
-      let imageUrl = `url(https://live.staticflickr.com/${serverId}/${id}_${secret}_${sizeSuffix}.jpg)`; 
 
-      console.log(imageUrl);
+      let total = data.photos.total;
+      for (let i = 0; i < total; i++) {
+        let randomNum = Math.ceil(Math.random() * 12) - 1;
 
-      let cit =document.querySelector('aside');
-      cit.style.backgroundImage = imageUrl;
-      
+        let id = data.photos.photo[randomNum].id;
+        let serverId = data.photos.photo[randomNum].server;
+        let secret = data.photos.photo[randomNum].secret;
+        let sizeSuffix = 'm';
 
+        let gameWrap = document.querySelector('.game-wrap')
+        let imageUrl = `url(https://live.staticflickr.com/${serverId}/${id}_${secret}_${sizeSuffix}.jpg)`;
+
+        for (j = 0; j < 2; j++) {
+          let memoryCard = document.createElement('aside')
+          gameWrap.appendChild(memoryCard)
+          memoryCard.style.backgroundImage = imageUrl;
+        }
+      }
+
+
+      /* 
+            let cit = document.querySelector('aside');
+            cit.style.backgroundImage = imageUrl; */
 
 
     }
-)
+  )
