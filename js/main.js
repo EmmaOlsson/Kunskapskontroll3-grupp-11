ba7d0edf8f0fcfea9ae0bf255548b1442e1a3fcf
mamounalchalabi <70330996+mamounalchalabi@users.noreply.github.com>
2020-12-01 12:17:53 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 fetch('https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=ede2a0d6e77e60346537e570cfab9800&gallery_id=72157717105466456&format=json&nojsoncallback=1?secret=1d5f8c611d8e8096?')
 .then(function(response){
 
