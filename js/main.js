e379827fffd257df5cab70759f6a9256da9dfdf7
Emma Olsson <emma.olsson22@learnet.se>
2020-12-01 15:54:04 +0100

    Updated js-loops

--- js/main.js
+++ js/main.js
@@ -10,16 +10,16 @@ const catGallery = '72157717105466456';
 const dogGallery = '72157717117782237';
 const horseGallery = '72157717118951277';
 
-
 // Selects and sets a variable for the gallery-buttons from HTML
 let catButton = document.querySelector('#cat-button')
 let dogButton = document.querySelector('#dog-button')
 let horseButton = document.querySelector('#horse-button')
 
 // Set this to a 
-function chooseGallery(){
+function chooseGallery() {
   return catGallery;
 }
+
 catButton.addEventListener('click', chooseGallery);
 
 
@@ -30,46 +30,45 @@ let urlGallery = chooseGallery();
 let url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${keyAPI}&gallery_id=${urlGallery}&format=json&nojsoncallback=1?secret=${keySecret}`;
 
 
-
 fetch(url)
-.then(function(response){
+  .then(function (response) {
 
-  if(response.status >= 200 && response.status <300){
+    if (response.status >= 200 && response.status < 300) {
       return response.json();
-  }
+    }
 
-})
-.then(
+  })
+  .then(
 
-    function(data){
+    function (data) {
       console.log(data)
 
-      let randomNum = Math.ceil(Math.random()*12)-1;
 
-      console.log(randomNum)
 
-      let id = data.photos.photo[randomNum].id;
-      let serverId = data.photos.photo[randomNum].server;
-      let secret = data.photos.photo[randomNum].secret;
-      let sizeSuffix = 'm';
+      let total = data.photos.total;
+      for (let i = 0; i < total; i++) {
+        let randomNum = Math.ceil(Math.random() * 12) - 1;
 
-      let gameWrap = document.querySelector('.game-wrap')
-      let imageUrl = `url(https://live.staticflickr.com/${serverId}/${id}_${secret}_${sizeSuffix}.jpg)`;
+        let id = data.photos.photo[randomNum].id;
+        let serverId = data.photos.photo[randomNum].server;
+        let secret = data.photos.photo[randomNum].secret;
+        let sizeSuffix = 'm';
 
-      for (i = 0; i < 24; i++) {
-        let memoryCard = document.createElement('aside')
-        gameWrap.appendChild(memoryCard)
-        memoryCard.style.backgroundImage = imageUrl;
+        let gameWrap = document.querySelector('.game-wrap')
+        let imageUrl = `url(https://live.staticflickr.com/${serverId}/${id}_${secret}_${sizeSuffix}.jpg)`;
+
+        for (j = 0; j < 2; j++) {
+          let memoryCard = document.createElement('aside')
+          gameWrap.appendChild(memoryCard)
+          memoryCard.style.backgroundImage = imageUrl;
+        }
+      }
+
+
+      /* 
+            let cit = document.querySelector('aside');
+            cit.style.backgroundImage = imageUrl; */
+
 
     }
-
-
-
-      console.log(imageUrl);
-/* 
-      let cit = document.querySelector('aside');
-      cit.style.backgroundImage = imageUrl; */
-
-
-    }
-)
+  )
