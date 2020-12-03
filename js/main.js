ec4cda9627d8e211b8adadacb04628ca29f70100
mamounalchalabi <70330996+mamounalchalabi@users.noreply.github.com>
2020-12-03 12:12:59 +0000

    last upd

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-
 // Setting the api-key to a variable
 const keyAPI = 'ede2a0d6e77e60346537e570cfab9800';
 
-//Setting thhe secret-key to a variable
+//Setting the secret-key to a variable
 const keySecret = '1d5f8c611d8e8096?';
 
 // Creating variables for different gallery-id:s
@@ -12,17 +11,22 @@ const horseGallery = '72157717118951277';
 
 // Selects and sets a variable for the gallery-buttons from HTML
 let catButton = document.querySelector('#cat-button')
-let dogButton = document.querySelector('#dog-button')
 let horseButton = document.querySelector('#horse-button')
+let dogButton = document.querySelector('#dog-button')
+
+// Skapar kort-objekt
+function Card(n, _serverId, _id, _secret){
+  this.imageNumber = n;
+  this.imageLink = `https://live.staticflickr.com/${_serverId}/${_id}_${_secret}_m.jpg`;
+}
 
 // Set this to a 
 function chooseGallery() {
+
   return catGallery;
 }
 
 catButton.addEventListener('click', chooseGallery);
-
-
 
 // If the user clicks on cat, the urlGallery = catGallery
 let urlGallery = chooseGallery();
@@ -31,44 +35,97 @@ let url = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhot
 
 
 fetch(url)
-  .then(function (response) {
+.then(function (response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response.json();
+  }
+})
+.then(
+  function (data) {
+    console.log(data)
 
-    if (response.status >= 200 && response.status < 300) {
-      return response.json();
+    let total = data.photos.total;
+
+    let sizeSuffix = 'm';
+
+    let gameWrap = document.querySelector('.game-wrap');
+
+    let cardDeck = []; //Kortleken.
+
+    for (let i = 0; i<2; i++){ // Lägger in samma bilder två gånger.
+      for (let j = 0; j < total; j++){ //Lägger in alla olika bilder i arrayn.
+        let id = data.photos.photo[j].id;
+        let serverId = data.photos.photo[j].server;
+        let secret = data.photos.photo[j].secret;
+        
+        console.log(id)
+
+        let card = new Card(j, serverId, id, secret);
+        cardDeck.push(card);
+      }
     }
+    cardDeck.sort( (a, b) => 0.5 - Math.random() ); //Blandar kortleken.
+    console.log(cardDeck);
 
-  })
-  .then(
+    for (let i = 0; i < cardDeck.length; i++){
+      let gameWrap = document.querySelector('.game-wrap');
+      let memoryCard = document.createElement('aside');
+      memoryCard.dataset.num=cardDeck[i].imageNumber;
+      memoryCard.classList.add('card');
+      memoryCard.style.backgroundImage = `url(${cardDeck[i].imageLink})`;
+      gameWrap.appendChild(memoryCard);
+      console.log(cardDeck[i].imageLink);
+      console.log(cardDeck[i].imageNumber)
+    };
+    console.log(gameWrap);
+    console.log(gameWrap.children);
 
-    function (data) {
-      console.log(data)
+    const card = document.querySelectorAll('aside');
+    let hasFlippedCard = false;
+    let firstCard, secondCard;
+    let score =1;
+    let tries =1;
+    function flipCard() {
+      this.classList='';
+      this.classList.add('card-show-image');
+      if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        
+      }else{
+        hasFlippedCard=false;
+        secondCard=this;
+        
+        console.log(firstCard.dataset.num)
+        console.log(secondCard.dataset.num)
 
-
-
-      let total = data.photos.total;
-      for (let i = 0; i < total; i++) {
-        let randomNum = Math.ceil(Math.random() * 12) - 1;
-
-        let id = data.photos.photo[randomNum].id;
-        let serverId = data.photos.photo[randomNum].server;
-        let secret = data.photos.photo[randomNum].secret;
-        let sizeSuffix = 'm';
-
-        let gameWrap = document.querySelector('.game-wrap')
-        let imageUrl = `url(https://live.staticflickr.com/${serverId}/${id}_${secret}_${sizeSuffix}.jpg)`;
-
-        for (j = 0; j < 2; j++) {
-          let memoryCard = document.createElement('aside')
-          gameWrap.appendChild(memoryCard)
-          memoryCard.style.backgroundImage = imageUrl;
+        if (firstCard.dataset.num === secondCard.dataset.num){
+          firstCard.removeEventListener('click', flipCard)
+          secondCard.removeEventListener('click', flipCard)
+          let scoreh = document.querySelector('.score');
+          scoreh.innerHTML=`Score: ${score}`
+          score++;
+          console.log(score)
+          let triesh = document.querySelector('.tries');
+          triesh.innerHTML=`Tries: ${tries}`
+          tries++;
+        }else{
+          firstCard.classList.remove('card-show-image')
+          secondCard.classList.remove('card-show-image')
+          let triesh = document.querySelector('.tries');
+          triesh.innerHTML=`Tries: ${tries}`
+          tries++;
         }
+        
       }
 
 
-      /* 
-            let cit = document.querySelector('aside');
-            cit.style.backgroundImage = imageUrl; */
-
-
     }
-  )
+    
+    card.forEach(card => card.addEventListener('click', flipCard));
+  }
+).catch(
+  error => {
+    console.log('Fel: ', error);
+  }
+);
